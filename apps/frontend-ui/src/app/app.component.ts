838e4b664bea2035eaf8dc0f04bb21611385ff68
YoniVi <yoni.vindelinckx@inetum-realdolmen.world>
2023-01-19 11:46:33 +0100

    create footer component

--- apps/frontend-ui/src/app/app.component.ts
+++ apps/frontend-ui/src/app/app.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // header
   appName = 'frontend-ui';
   list = ['dataTable', 'footer', 'dropdown', 'form'];
   logo = 'assets/IMG.jpg';
+
+  // footer
+  facebookSocialLinks = 'https://www.facebook.com/yoni.vindelinckx/';
+  twitterSocialLinks = 'https://twitter.com/elonmusk?';
+  linkHeaderOptions = ['link1', 'link2', 'link3'];
+  linkHeaders = ['test1', 'test2', 'test3'];
+  copyRightsName = 'yoni vindelinckx';
+  description =
+    'Het is al geruime tijd een bekend gegeven dat een lezer, tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters';
 }
--- libs/components/src/lib/footer/footer.component.ts
+++ libs/components/src/lib/footer/footer.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-lib-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  @Input() footerDescription: string | undefined;
+  @Input() copyRightsName: string | undefined;
+  @Input() linkHeaders: string[] | undefined;
+  @Input() linkHeaderOptions: string[] | undefined;
+  @Input() version: string | undefined;
+
+  // social links here
+  @Input() facebookSocialLink: string | undefined;
+  @Input() linkedInSocialLink: string | undefined;
+  @Input() instagramSocialLink: string | undefined;
+  @Input() twitterSocialLink: string | undefined;
+}
