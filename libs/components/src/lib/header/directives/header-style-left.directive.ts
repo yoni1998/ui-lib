86a57ba4d6e4129db0a00ba72a8393cc854c9ff6
YoniVi <yoni.vindelinckx@inetum-realdolmen.world>
2023-01-27 12:35:14 +0100

    add media queries for smartphone, tablet and deskt

--- libs/components/src/lib/header/directives/header-style-left.directive.ts
+++ libs/components/src/lib/header/directives/header-style-left.directive.ts
@@ -6,5 +6,11 @@ import { Directive, ElementRef } from '@angular/core';
 export class HeaderStyleLeftDirective {
   constructor(private el: ElementRef) {
     el.nativeElement.style.float = 'left';
+    if (el.nativeElement.localName === 'img') {
+      el.nativeElement.style.display = 'inline-block';
+    }
+    if (el.nativeElement.localName === 'h1') {
+      el.nativeElement.style.padding = '1rem';
+    }
   }
 }
--- libs/components/src/lib/header/directives/header-style-right.directive.ts
+++ libs/components/src/lib/header/directives/header-style-right.directive.ts
@@ -6,6 +6,9 @@ import { Directive, ElementRef } from '@angular/core';
 export class HeaderStyleRightDirective {
   constructor(private el: ElementRef) {
     el.nativeElement.style.float = 'right';
+    if (el.nativeElement.localName === 'img') {
+      el.nativeElement.style.display = 'inline-block';
+    }
     if (el.nativeElement.localName === 'h1') {
       el.nativeElement.style.padding = '1rem';
     }
